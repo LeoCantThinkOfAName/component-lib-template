9f968782244a6d04ca21cbe9b9e64e83ecc759cf
leocantthinkofaname <i7851@hotmail.com>
2021-10-17 17:31:22 +0800

    seperate script when running ci

--- build.sh
+++ build.sh
@@ -10,41 +10,52 @@ index="./src/index.ts"
 cols=$(tput cols)
 errors=0
 
-function repeat_chracter {
-  prepends=""
-  for ((n=0;n<$1;n++))
-  do
-    prepends+=$2
+if [ CI = true ]; then
+  for f in $path/*
+    do
+      component=${f#"$path/"}
+      if grep -q $component "$index"; then
+        echo $component exported
+      else
+        exit 1
+      fi
   done
-}
-
-repeat_chracter cols "="
-echo -e "$prepends\nChecking exports...\n$prepends\n"
-
-for f in $path/*
-do
-  component=${f#"$path/"}
-  if grep -q $component "$index"; then
-    result="${component} is exported "
-    width=$((cols - ${#result} - 3))
-    repeat_chracter width "-"
-    echo -e "$result${GREEN}$prepends${NC} 👌"
-  else
-    result="${component} is not exported "
-    width=$((cols - ${#result} - 3))
-    repeat_chracter width "-"
-    errors=$((errors + 1))
-    echo -e "$result${RED}$prepends${NC} 💀"
-  fi
-done
-
-if [ $errors -ne 0 ]; then
-  repeat_chracter cols "="
-  echo -e "\n$prepends"
-  echo -e "You have ${RED}$errors${NC} component(s) not exported."
-  exit 1
 else
-  clear
-  # Start building process
-  yarn rollup -c
+  function repeat_chracter {
+    prepends=""
+    for ((n=0;n<$1;n++))
+    do
+      prepends+=$2
+    done
+  }
+
+  repeat_chracter cols "="
+  echo -e "$prepends\nChecking exports...\n$prepends\n"
+
+  for f in $path/*
+    do
+      component=${f#"$path/"}
+      if grep -q $component "$index"; then
+        result="${component} is exported "
+        width=$((cols - ${#result} - 3))
+        repeat_chracter width "-"
+        echo -e "$result${GREEN}$prepends${NC} 👌"
+      else
+        result="${component} is not exported "
+        width=$((cols - ${#result} - 3))
+        repeat_chracter width "-"
+        errors=$((errors + 1))
+        echo -e "$result${RED}$prepends${NC} 💀"
+      fi
+  done
+
+  if [ $errors -ne 0 ]; then
+    repeat_chracter cols "="
+    echo -e "\n$prepends"
+    echo -e "You have ${RED}$errors${NC} component(s) not exported."
+    exit 1
+  else
+    # Start building process
+    yarn rollup -c
+  fi
 fi
