e80cf4f51af7c29d122753b05c435a3e224011a7
leocantthinkofaname <i7851@hotmail.com>
2021-10-17 17:52:00 +0800

    testing

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ path="./src/components"
 
 if [ CI = true ]; then
   for f in $path/*
-    do
-      component=${f#"$path/"}
-      if grep -q $component "$index"; then
-        echo $component exported
-      else
-        exit 1
-      fi
+  do
+    component=${f#"$path/"}
+    if grep -q $component "$index"; then
+      echo $component exported
+    else
+      exit 1
+    fi
   done
 else
   clear
@@ -21,40 +21,36 @@ else
   cols=$(tput cols)
   errors=0
 
+  function repeat_chracter {
+    prepends=""
+    for ((n = 0; n < $1; n++))
+    do
+      prepends+=$2
+    done
+  }
+
   repeat_chracter cols "="
   echo -e "$prepends\nChecking exports...\n$prepends\n"
 
   for f in $path/*
-    do
-      component=${f#"$path/"}
-      if grep -q $component "$index"; then
-        result="${component} is exported "
-        width=$((cols - ${#result} - 3))
-        prepends=""
-        for ((n=0;n<$width;n++))
-        do
-          prepends+="-"
-        done
-        echo -e "$result${GREEN}$prepends${NC} 👌"
-      else
-        result="${component} is not exported "
-        width=$((cols - ${#result} - 3))
-        prepends=""
-        for ((n=0;n<$width;n++))
-        do
-          prepends+="-"
-        done
-        errors=$((errors + 1))
-        echo -e "$result${RED}$prepends${NC} 💀"
-      fi
+  do
+    component=${f#"$path/"}
+    if grep -q $component "$index"; then
+      result="${component} is exported "
+      width=$((cols - ${#result} - 3))
+      repeat_chracter width "-"
+      echo -e "$result${GREEN}$prepends${NC} 👌"
+    else
+      result="${component} is not exported "
+      width=$((cols - ${#result} - 3))
+      repeat_chracter width "-"
+      errors=$((errors + 1))
+      echo -e "$result${RED}$prepends${NC} 💀"
+    fi
   done
 
   if [ $errors -ne 0 ]; then
-    prepends=""
-    for ((n=0;n<$cols;n++))
-    do
-      prepends+="-"
-    done
+    repeat_chracter cols "="
     echo -e "\n$prepends"
     echo -e "You have ${RED}$errors${NC} component(s) not exported."
     exit 1
